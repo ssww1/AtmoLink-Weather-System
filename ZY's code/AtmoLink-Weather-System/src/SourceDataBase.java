25a39533821a0e2bcc3238955cba6a6f883139fd
NPC00101 <202383930030@nuist.edu.cn>
2023-11-20 21:10:08 +0800

    1

--- ZY's code/AtmoLink-Weather-System/src/SourceDataBase.java
+++ ZY's code/AtmoLink-Weather-System/src/SourceDataBase.java
@@ -1,2 +1,54 @@
 public class SourceDataBase {
+    //arrays of each value,each number stands for one day
+    //eg:[0]stands for data of day1
+    SourceDataBase() {
+        String[] weather = new String[7];
+        weather[0] = "晴朗";
+        weather[1] = "多云";
+        weather[2] = "小雨";
+        weather[3] = "暴雨";
+        weather[4] = "晴朗";
+        weather[5] = "小雪";
+        weather[6] = "大雪";
+
+
+        double[] temperature = new double[7];
+        temperature[0] = 10.8;
+        temperature[1] = 8.0;
+        temperature[2] = 5.0;
+        temperature[3] = 4.5;
+        temperature[4] = 8.0;
+        temperature[5] = -2.0;
+        temperature[6] = -10.0;
+
+        int[] windforce = new int[7];
+        windforce[0] = 2;
+        windforce[1] = 3;
+        windforce[2] = 2;
+        windforce[3] = 4;
+        windforce[4] = 2;
+        windforce[5] = 3;
+        windforce[6] = 1;
+
+        double[] humidity = new double[7];
+        humidity[0] = 4.8;
+        humidity[1] = 8.3;
+        humidity[2] = 10.8;
+        humidity[3] = 15.7;
+        humidity[4] = 3.3;
+        humidity[5] = 9.0;
+        humidity[6] = 9.9;
+
+        String[] barometric = new String[7];
+        barometric[0] = "1018hpa";
+        barometric[1] = "1028hpa";
+        barometric[2] = "1076hpa";
+        barometric[3] = "1172hpa";
+        barometric[4] = "1009hpa";
+        barometric[5] = "1017hpa";
+        barometric[6] = "1016hpa";
+
+    }
 }
+
+
--- ZY's code/AtmoLink-Weather-System/src/User.java
+++ ZY's code/AtmoLink-Weather-System/src/User.java
@@ -1,2 +1,39 @@
-public class User {
+import java.util.Scanner;
+public class User extends SourceDataBase {
+    int date;
+    int kindOfData;
+    public void Userimplementationinterface(){
+            System.out.println("AtmoLink-Weather-System v1.0");
+            System.out.println("Please press enter to continue");
+            java.util.Scanner input = new Scanner(System.in);
+
+
+        }
+    public void acquireData(){
+
+        //get the date
+        System.out.println("Please enter the data you want to consult,you should enter number from 1 to 7");
+        java.util.Scanner input = new Scanner(System.in);
+        int date = input.nextInt();
+        //get the kind of data user wants to geet
+        System.out.println("Please enter the kind of data you want to get");
+        int kindOfData = input.nextInt();
+
+        if (date == 1){
+            switch (kindOfData){
+                
+
+            }
+        }
+
+
+    }
 }
+
+
+
+
+
+
+
+
