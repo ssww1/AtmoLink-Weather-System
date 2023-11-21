e3947157cda848a3f27a4117e6de579216ddec73
NPC00101 <202383930030@nuist.edu.cn>
2023-11-21 20:44:06 +0800

    new

--- ZY's code/AtmoLink-Weather-System/src/SourceDataBase.java
+++ ZY's code/AtmoLink-Weather-System/src/SourceDataBase.java
@@ -1,68 +1,63 @@
 public class SourceDataBase {
 }
 
-class weather{
-    public static void main(String[] args) {
-        String[] weather = new String[7];
-        weather[0] = "sunny";
-        weather[1] = "cloudy";
-        weather[2] = "drizzle";
-        weather[3] = "rainstorm";
-        weather[4] = "sunny";
-        weather[5] = "snow";
-        weather[6] = "snowstorms";
+class Weather{
+    private String[] weather = {"sunny","cloudy","drizzle","rainstorm","snow","snowstorms"};
+    public String[] getWeather() {
+        return weather;
+    }
+    public void setWeather(String[] weather) {
+        this.weather = weather;
     }
 }
 
-class temperature{
-    public static void main(String[] args) {
-        double[] temperature = new double[7];
-        temperature[0] = 10.8;
-        temperature[1] = 8.0;
-        temperature[2] = 5.0;
-        temperature[3] = 4.5;
-        temperature[4] = 8.0;
-        temperature[5] = -2.0;
-        temperature[6] = -10.0;
+class Temperature{
+
+        double[] temperature = {10.8,8.0,5.0,4.5,8.0,-2.0,-10.0};
+        public double[] getTemperature() {
+        return temperature;
+        }
+        public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+        }
+}
+
+class Windforce{
+    int[] windforce = {2,3,2,4,2,3,1};
+
+    public int[] getWindforce() {
+        return windforce;
+    }
+
+    public void setWindforce(int[] windforce) {
+        this.windforce = windforce;
+    }
+}
+class Humidity{
+
+        double[] humidity = {4.8,8.3,10.8,15.7,3.3,9.0,9.9};
+
+    public void setHumidity(double[] humidity) {
+        this.humidity = humidity;
+    }
+
+    public double[] getHumidity() {
+        return humidity;
     }
 }
 
-class windforce{
-    public static void main(String[] args) {
-        int[] windforce = new int[7];
-        windforce[0] = 2;
-        windforce[1] = 3;
-        windforce[2] = 2;
-        windforce[3] = 4;
-        windforce[4] = 2;
-        windforce[5] = 3;
-        windforce[6] = 1;
-    }
-}
-class humidity{
-    public static void main(String[] args) {
-        double[] humidity = new double[7];
-        humidity[0] = 4.8;
-        humidity[1] = 8.3;
-        humidity[2] = 10.8;
-        humidity[3] = 15.7;
-        humidity[4] = 3.3;
-        humidity[5] = 9.0;
-        humidity[6] = 9.9;
-    }
-}
+class Barometric{
 
-class barometric{
-    public static void main(String[] args) {
-        String[] barometric = new String[7];
-        barometric[0] = "1018hpa";
-        barometric[1] = "1028hpa";
-        barometric[2] = "1076hpa";
-        barometric[3] = "1172hpa";
-        barometric[4] = "1009hpa";
-        barometric[5] = "1017hpa";
-        barometric[6] = "1016hpa";
-    }}
+        String[] barometric = {"1018hpa","1028hpa","1076hpa","1172hpa","1009hpa","1017hpa","1016hpa"};
+
+    public String[] getBarometric() {
+        return barometric;
+    }
+
+    public void setBarometric(String[] barometric) {
+        this.barometric = barometric;
+    }
+}
 
 
 
