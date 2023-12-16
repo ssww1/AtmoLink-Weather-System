5cde1cb7b3276660a158aeaf2421cff1bdb29477
666Charles666 <147719423+666Charles666@users.noreply.github.com>
2023-12-16 19:00:14 +0800

    Add files via upload

---
+++ ZRC code/src/Login.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Login {
+    String username;
+    String[] users = new String[10];
+    String[] passwords = new String[10];
+
+    public void getUsername() {
+        this.username = Userlogin();
+    }
+
+    //give initial user's name and user's password
+    public void InitializeDatabase() {
+
+        users[0] = "Administrator";
+        passwords[0] = "1234";
+
+        users[1] = "User";
+        passwords[1] = "0000";
+    }
+
+    public String Userlogin() {
+        InitializeDatabase();
+
+        String result = "login failed";
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please enter your username:" + "(please enter Administrator or User)");
+            String enteredUsername = input.next();
+
+            System.out.println("Please enter your password:" + "(it should include 4 characters)");
+            String password = input.next();
+
+            for (int i = 0; i < 10; i++) {
+                if (enteredUsername.equals(users[i]) && password.equals(passwords[i])) {
+                    result = enteredUsername;
+                    return result; // exit the method when credentials are correct
+                }
+            }
+
+            System.out.println("User name or password wrong, please try again");
+        }
+    }
+}
+
+
+
+
+
